b602408afbe5df56988e86e5e9cbf953736ccaf0
Yuri Baranov <baranovu@gmail.com>
2017-06-11 14:37:03 +0300

    ctor and state for Log

--- app/javascript/components/log.jsx
+++ app/javascript/components/log.jsx
@@ -1,13 +1,18 @@
-console.log('hh');
 import React from 'react';
-console.log("init log");
-class Log extends React.Component {
+
+export default class Log extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {log_event_list: this.props.log_events};
+        this.defaultProps = {log_event_list:[]};
+    }
     render() {
         console.log("Log");
+        console.log(this);
         return <h2>
             Log :
         </h2>
     }
 }
 
-export default Log;
+
