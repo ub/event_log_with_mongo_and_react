24929c5bdca8622b7c782118f0dcd8926d5f009e
Yuri Baranov <baranovu@gmail.com>
2017-06-11 21:11:41 +0300

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :log_events
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :log_events, only: 'index'
+  root to: 'log_events#index'
 end
