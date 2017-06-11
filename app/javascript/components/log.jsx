6c36cf62e4f5562e657b9130a601c85099ba5890
Yuri Baranov <baranovu@gmail.com>
2017-06-11 17:55:52 +0300

    task type filter

--- app/javascript/components/log.jsx
+++ app/javascript/components/log.jsx
@@ -4,43 +4,45 @@ import LogEvent from 'components/log_event';
 export default class Log extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {log_event_list: this.props.log_events};
-        this.defaultProps = {log_event_list:[]};
+        this.state = {log_event_list: this.props.log_events, task_filter: null};
+        this.defaultProps = {log_event_list: []};
     }
+
     render() {
         console.log("Log");
         console.log(this);
         return (
-        <div className="container mt-5">
-            <h2>
-                Log :
-            </h2>
-            <table className='table table-bordered table-sm'>
-                <thead>
-                <tr>
-                    <th className="w-10">Хост</th>
-                    <th className="w-20">Время</th>
-                    <th className="w-10">Длит.</th>
-                    <th className="w-10">Тип</th>
-                    <th className="w-50">Сообщение</th>
-                </tr>
-                </thead>
-                <tbody>
-                {this.state.log_event_list.map((le) =>
-                    {
+            <div className="container mt-5">
+                <h2>
+                    Log :
+                </h2>
+                <table className='table table-bordered table-sm'>
+                    <thead>
+                    <tr>
+                        <th className="w-10">Хост</th>
+                        <th className="w-20">Время</th>
+                        <th className="w-10">Длит.</th>
+                        <th className="w-10">Тип</th>
+                        <th className="w-50">Сообщение</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.log_event_list.map((le) => {
                         console.log(le);
                         console.log(le._id['$oid']);
-                    let duration = le.time_end - le.time_start;
-                    let time_start = (new Date(le.time_start * 1000)).toLocaleString('ru');
-                    return <LogEvent key={le._id['$oid']} host={le.host} result_type={le.result_type}
-                            task_type={le.task_type} time_start={time_start} duration={duration}
-                       result_message={le.result_message}/>;
-                })}
+                        let duration = le.time_end - le.time_start;
+                        let time_start = (new Date(le.time_start * 1000)).toLocaleString('ru');
+                        let pass_filter = (this.state.task_filter == null) || (this.state.task_filter == le.task_type);
+                        return <LogEvent key={le._id['$oid']} host={le.host} result_type={le.result_type}
+                                         task_type={le.task_type} time_start={time_start} duration={duration}
+                                         hide={!pass_filter}
+                                         result_message={le.result_message}/>;
+                    })}
 
-                </tbody>
-            </table>
-        </div>
-                )
+                    </tbody>
+                </table>
+            </div>
+        )
     }
 }
 
--- app/javascript/components/log_event.jsx
+++ app/javascript/components/log_event.jsx
@@ -4,17 +4,20 @@ export default class LogEvent extends React.Component {
     constructor(props) {
         super(props);
     }
+
     render() {
-        //console.log(this.props);
         let bg_class = this.props.result_type == 'error' ? 'bg-danger' : 'bg-success';
+        if (this.props.hide) return null;
         return (
-        <tr className={bg_class}>
-            <td>{this.props.host}</td>
-            <td><small>{this.props.time_start}</small></td>
-            <td>{this.props.duration}</td>
-            <td>{this.props.task_type}</td>
-            <td>{this.props.result_message}</td>
-        </tr>);
+            <tr className={bg_class}>
+                <td>{this.props.host}</td>
+                <td>
+                    <small>{this.props.time_start}</small>
+                </td>
+                <td>{this.props.duration}</td>
+                <td>{this.props.task_type}</td>
+                <td>{this.props.result_message}</td>
+            </tr>);
     }
 
 }
