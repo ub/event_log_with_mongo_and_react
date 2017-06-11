f05052685ecf1cccbab0a23d8780e6ac3c69be5b
Yuri Baranov <baranovu@gmail.com>
2017-06-11 18:45:11 +0300

    result type filter implemented debug logging removed

--- app/javascript/components/log.jsx
+++ app/javascript/components/log.jsx
@@ -4,45 +4,57 @@ import LogEvent from 'components/log_event';
 export default class Log extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {log_event_list: this.props.log_events, task_filter: null};
+        this.state = {log_event_list: this.props.log_events, task_filter: null, result_filter: null};
         this.defaultProps = {log_event_list: []};
     }
 
     handleTaskTypeFilterChange(ev) {
-        console.log(ev);
-        console.log(ev.target.value);
-        if(!ev.target.value) console.log("blank");
-
-        if(!ev.target.value) {
+        if (!ev.target.value) {
             this.setState({task_filter: null});
         } else {
             this.setState({task_filter: ev.target.value});
         }
-        console.log('state');
-        console.log(this.state.task_filter);
-
     }
+    
+    handleResultTypeFilterChange(ev) {
+        if (!ev.target.value) {
+            this.setState({result_filter: null});
+        } else {
+            this.setState({result_filter: ev.target.value});
+        }
+    }
+
     renderTaskTypeFilter() {
         return (
-        <select className="custom-select" defaultValue={null} onChange={(e) => this.handleTaskTypeFilterChange(e)}>
-            <option value="" >Фильтр по типу задачи</option>
-            <option >task_1</option>
-            <option >task_2</option>
-            <option >task_3</option>
-            <option >task_4</option>
-            <option >task_5</option>
-        </select>
-            );
+            <select className="custom-select" defaultValue={null} onChange={(e) => this.handleTaskTypeFilterChange(e)}>
+                <option value="">Фильтр по типу задачи</option>
+                <option >task_1</option>
+                <option >task_2</option>
+                <option >task_3</option>
+                <option >task_4</option>
+                <option >task_5</option>
+            </select>
+        );
+    }
+
+    renderResultTypeFilter() {
+        return (
+            <select className="custom-select" defaultValue={null} onChange={(e) => this.handleResultTypeFilterChange(e)}>
+                <option value="">Фильтр по результату</option>
+                <option >success</option>
+                <option >error</option>
+            </select>
+        );
     }
 
     render() {
-        console.log(this);
         return (
             <div className="container mt-5">
                 <h2>
                     Журнал событий
                 </h2>
                 {this.renderTaskTypeFilter()}
+                {this.renderResultTypeFilter()}
                 <hr />
                 <table className='table table-bordered table-sm'>
                     <thead>
@@ -56,11 +68,11 @@ export default class Log extends React.Component {
                     </thead>
                     <tbody>
                     {this.state.log_event_list.map((le) => {
-                        console.log(le);
-                        console.log(le._id['$oid']);
                         let duration = le.time_end - le.time_start;
                         let time_start = (new Date(le.time_start * 1000)).toLocaleString('ru');
                         let pass_filter = (this.state.task_filter == null) || (this.state.task_filter == le.task_type);
+                        pass_filter = pass_filter && ((this.state.result_filter == null) || this.state.result_filter == le.result_type);
+
                         return <LogEvent key={le._id['$oid']} host={le.host} result_type={le.result_type}
                                          task_type={le.task_type} time_start={time_start} duration={duration}
                                          hide={!pass_filter}
